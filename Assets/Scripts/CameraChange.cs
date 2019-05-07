ad0976f96b2c056852f8de835e3fe6309983f28d
Lenore Watson <lenore@cidesigninc.com>
2019-05-07 13:13:17 -0500

    second initial commit

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public Camera ThirdPersonCamera;
+    public Camera FirstPersonCamera;
+
+    void Start()
+    {
+        ThirdPersonCamera.enabled = true;
+        FirstPersonCamera.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            ThirdPersonCamera.enabled = true;
+            FirstPersonCamera.enabled = false;
+        }
+        if (Input.GetKeyDown("f"))
+        {
+            FirstPersonCamera.enabled = true;
+            ThirdPersonCamera.enabled = false;
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Unity-Virtual-Campus-Tour
-A 3D "game" where a player walks through a campus, i.e., a virtual tour.
